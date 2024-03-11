9e93c14ba047913ca3aa2159911fb2cdc9537b05
JulianManera8 <julianmanera@hotmail.com>
2024-03-11 12:42:13 -0300

    estructura inicial

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import VueLazyLoad from 'vue3-lazyload'
+//importar algun icono asi
+//import { GiMushroomCloud } from "oh-vue-icons/icons";
+//agregarle a esta funcion el icono q queramos agg
+addIcons();
 
-createApp(App).use(router).mount('#app')
+
+const app = createApp(App)
+app.use(router)
+app.use(VueLazyLoad)
+app.component('v-icon', OhVueIcon)
+app.mount('#app')
---
+++ src/views/FiguraView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <FiguraComp />
+    </div>
+</template>
+
+<script setup>
+  import FiguraComp from '@/components/FiguraComp.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
